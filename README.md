b78c224c3d4b72720fe12b46b5c9a8f41773d98d
SOO YANG, TAN <sooyang.code@gmail.com>
2019-01-26 17:13:09 +0800

    Add reporting method

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Simple project to simulate the speed of querying and counting.
 - `bundle install`
 - Enter `rails c`
 - Seed the data for example `1000.times {Project.new(status: [0,1].sample, team_lead_action_time: [Time.current, nil].sample, manager_action_time: [Time.current, nil].sample).save}`
-- Run the following methods to get the benchmark value `Project.count_active_projects` and `Project.count_active_projects_ar` and `Project.count_active_projects_sql`
+- Run the following methods to get the benchmark value `Project.report`
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,38 +2,37 @@ class Project < ApplicationRecord
   enum status: [ :active, :archived ]
 
   def self.count_active_projects
-    time = Benchmark.measure {
-      active_projects = Project.where(status: 'active')
-      @total_number_of_active_projects = active_projects.size
-      @projects_pending_team_lead_approval = active_projects.reject(&:team_lead_action_time).count
-      @projects_pending_manager_approval = active_projects.reject(&:manager_action_time).count
-    }
-    puts time
+    active_projects = Project.where(status: 'active')
+    @total_number_of_active_projects = active_projects.size
+    @projects_pending_team_lead_approval = active_projects.reject(&:team_lead_action_time).count
+    @projects_pending_manager_approval = active_projects.reject(&:manager_action_time).count
   end
 
   def self.count_active_projects_ar
-    time = Benchmark.measure {
-      active_projects = Project.where(status: 'active')
-      @total_number_of_active_projects = active_projects.size
-      @projects_pending_team_lead_approval = active_projects.where(team_lead_action_time: nil).size
-      @projects_pending_manager_approval = active_projects.where(manager_action_time: nil).size
-    }
-    puts time
+    active_projects = Project.where(status: 'active')
+    @total_number_of_active_projects = active_projects.size
+    @projects_pending_team_lead_approval = active_projects.where(team_lead_action_time: nil).size
+    @projects_pending_manager_approval = active_projects.where(manager_action_time: nil).size
   end
 
   def self.count_active_projects_sql
-    time = Benchmark.measure {
-      query = <<~SQL
-        SELECT
-          SUM(CASE WHEN status = 0 THEN 1 ELSE 0 END),
-          SUM(CASE WHEN status = 0 AND team_lead_action_time IS null THEN 1 ELSE 0 END),
-          SUM(CASE WHEN status = 0 AND manager_action_time IS null THEN 1 ELSE 0 END)
-        FROM
-          PROJECTS
-      SQL
+    query = <<~SQL
+      SELECT
+        SUM(CASE WHEN status = 0 THEN 1 ELSE 0 END),
+        SUM(CASE WHEN status = 0 AND team_lead_action_time IS null THEN 1 ELSE 0 END),
+        SUM(CASE WHEN status = 0 AND manager_action_time IS null THEN 1 ELSE 0 END)
+      FROM
+        PROJECTS
+    SQL
 
-      ActiveRecord::Base.connection.execute(query)
-    }
-    puts time
+    ActiveRecord::Base.connection.execute(query)
+  end
+
+  def self.report
+    Benchmark.bmbm do |x|
+      x.report("count_active_project") { Project.count_active_projects }
+      x.report("count_active_projects_ar") { Project.count_active_projects_ar }
+      x.report("count_active_projects_sql") { Project.count_active_projects_sql }
+    end
   end
 end
